31383a19500bbb80803e8963e37f272662e2b435
Thiago Elias <thiagoelias99@gmail.com>
2023-11-04 08:55:29 -0300

    feat: added header responsibility

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,11 +5,11 @@ import Links from './links'
 
 export default function index() {
     return (
-        <header className=' mx-auto absolute w-screen h-24 z-50 p-4 bg-black'>
-            <div className='flex flex-row w-full h-full justify-between items-end max-w-[1024px] mx-auto'>
+        <header className=' mx-auto absolute w-screen h-32 md:h-24 z-50 p-4 pb-0 md:pb-4 bg-black'>
+            <div className='flex flex-col md:flex-row w-full h-full justify-between items-center md:items-end max-w-[1024px] mx-auto'>
                 <Logo />
                 <Links />
-                <Search />
+                {/* <Search /> */}
             </div>
         </header>
     )
