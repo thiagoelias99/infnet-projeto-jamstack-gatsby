e5dbec19d612c1605beef401cdc4b83b04834786
Thiago Elias <thiagoelias99@gmail.com>
2023-11-04 10:50:13 -0300

    feat: added post page responsively

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import Layout from '@/components/layout'
 
-const MyH1 = (props: React.HTMLProps<HTMLHeadingElement>) => <h1 className='text-black text-5xl my-4 text-center w-full' {...props} />
-const MyH2 = (props: React.HTMLProps<HTMLHeadingElement>) => <h2 className='text-black text-3xl mt-16 mb-4 p-4 text-center w-full bg-slate-100 rounded' {...props} />
+const MyH1 = (props: React.HTMLProps<HTMLHeadingElement>) => <h1 className='text-black text-3xl md:text-4xl lg:text-5xl my-4 text-center max-w-full' {...props} />
+const MyH2 = (props: React.HTMLProps<HTMLHeadingElement>) => <h2 className='text-black text-xl font-bold md:text-2xl lg:text-3xl mt-16 mb-4 p-4 text-center w-full bg-slate-100 rounded' {...props} />
 const MyH3 = (props: React.HTMLProps<HTMLHeadingElement>) => <h3 className='text-black text-xl mt-2 mb-4 font-bold' {...props} />
-const Myp = (props: React.HTMLProps<HTMLParagraphElement>) => <p className='text-black text-base' {...props} />
+const Myp = (props: React.HTMLProps<HTMLParagraphElement>) => <p className='text-slate-600 text-base' {...props} />
 const MyUl = (props: React.HTMLProps<HTMLUListElement>) => <ul className='text-black text-base list-disc px-8' {...props} />
-const MyLi = (props: React.HTMLProps<HTMLLIElement>) => <li className='text-black text-base' {...props} />
+const MyLi = (props: React.HTMLProps<HTMLLIElement>) => <li className='text-slate-600 text-base' {...props} />
 const MyStrong = (props: React.HTMLProps<HTMLSpanElement>) => <span className='text-black font-semibold text-lg' {...props} />
-const MyImg = (props: React.HTMLProps<HTMLImageElement>) => <img className='max-w-[560px] mx-auto mb-4 rounded-xl' {...props} />
+const MyImg = (props: React.HTMLProps<HTMLImageElement>) => <img className='mx-auto mb-4 rounded-xl w-full lg:w-10/12' {...props} />
 const MyA = (props: React.HTMLProps<HTMLAnchorElement>) => <a className='text-black text-base' {...props} />
-const MyBlockquote = (props: React.HTMLProps<HTMLQuoteElement>) => <blockquote className='text-black px-8 py-16 m-auto text-2xl font-bold text-justify w-full mb-[-40px] bg-slate-100 rounded shadow' {...props} />
+const MyBlockquote = (props: React.HTMLProps<HTMLQuoteElement>) => <blockquote className='text-slate-900 px-8 py-10 md:py-16 m-auto text-lg md:text-xl lg:text-2xl font-bold text-justify w-full mb-[-40px] bg-slate-100 rounded shadow' {...props} />
 
 
 const components = {
@@ -66,7 +66,7 @@ export default function Post({ data, children }: IProps) {
   return (
     <Layout>
       <ComponentsWrapper>
-        <div className='max-w-[1024px] m-auto mt-32'>{children}</div>
+        <article className='w-screen max-w-[1024px] m-auto mt-32 p-4'>{children}</article>
       </ComponentsWrapper>
     </Layout>
   )
