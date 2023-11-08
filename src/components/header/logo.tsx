99726cbf49fd931db045f86614712daf12a55894
Thiago Elias <thiagoelias99@gmail.com>
2023-11-08 13:36:39 -0300

    feat: adding image optimization

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo1 from '../../images/Email Address.svg'
-import logo2 from '../../images/Shape.svg'
+// import logo1 from '../../images/Email Address.svg'
+// import logo2 from '../../images/Shape.svg'
+import { StaticImage } from 'gatsby-plugin-image'
 
 export default function Logo() {
   return (
     <Link to='/'>
       <div className='flex flex-row max-h-12'>
-        <img src={logo1} alt="" />
-        <img src={logo2} alt="" />
+        {/* <img src={logo1} alt="" /> */}
+        <StaticImage
+          src='../../images/Email Address.svg'
+          alt='logo'
+          placeholder="none"
+          objectFit='contain'
+        />
+        {/* <img src={logo2} alt="" /> */}
+        <StaticImage
+          src='../../images/Shape.svg'
+          alt='logo'
+          placeholder="none"
+          objectFit='contain'
+        />
       </div>
     </Link>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
-import { graphql, Link, type HeadFC, type PageProps } from "gatsby"
+import { graphql, type HeadFC, type PageProps } from "gatsby"
 import Layout from "@/components/layout"
-import Cover from "../images/home-cover.png"
 import Card from '@/components/card'
+import { StaticImage } from "gatsby-plugin-image"
 
 export const query = graphql`
     query {
@@ -42,29 +42,37 @@ const IndexPage: React.FC<PageProps> = ({ data }) => {
 
   return (
     <Layout>
-      <figure className='flex max-h-[900px] relative mt-32 md:mt-24'>
-        <div className='w-full absolute top-[40%] text-center z-20'>
-          <p className='text-2xl md:text-5xl text-white uppercase'>inspirações para viajar</p>
-        </div>
-        <div className='absolute w-full h-full bg-black/25 z-10'></div>
-        <img src={Cover} alt="" className='max-h-[900px] z-0 bg-cover w-full' />
-      </figure>
+      <main>
+        <figure className='flex max-h-[900px] relative mt-32 md:mt-24'>
+          <div className='w-full absolute top-[40%] text-center z-20'>
+            <p className='text-2xl md:text-5xl text-white uppercase'>inspirações para viajar</p>
+          </div>
+          <div className='absolute w-full h-full bg-black/25 z-10'></div>
+          {/* <img src={Cover} alt="" className='max-h-[900px] z-0 bg-cover w-full' /> */}
+          <StaticImage
+            src='../images/home-cover.png'
+            alt='mountains'
+            className='max-h-[900px] z-0 bg-cover w-full'
+            placeholder="blurred"
+          />
+        </figure>
 
-      <section className='p-4 max-w-[1024px] m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10 gap-y-10'>
-        <h2 className='col-span-1 md:col-span-2 lg:col-span-3 text-3xl md:text-4xl lg:text-5xl text-center mt-4'>Últimos Posts</h2>
+        <section className='p-4 max-w-[1024px] m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-10 gap-y-10'>
+          <h2 className='col-span-1 md:col-span-2 lg:col-span-3 text-3xl md:text-4xl lg:text-5xl text-center mt-4'>Últimos Posts</h2>
 
-        {queryData.allMdx.nodes.map((post) => {
-          return (
-            <Card key={post.id}
+          {queryData.allMdx.nodes.map((post) => {
+            return (
+              <Card key={post.id}
               title={post.frontmatter.title}
               cover={post.frontmatter.hero}
               date={post.frontmatter.date}
               description={post.excerpt.replace('Introdução ', '')}
               slug={post.frontmatter.slug}
-            />
-          )
-        })}
-      </section>
+              />
+            )
+          })}
+        </section>
+      </main>
 
       <form method="post" name="subscriptions" data-netlify="true" data-netlify-honeypot="bot-field" hidden>
         <input type="hidden" name="form-name" value="subscriptions" />
@@ -77,6 +85,7 @@ const IndexPage: React.FC<PageProps> = ({ data }) => {
         <input type="hidden" name="monthlyResume" />
       </form>
     </Layout>
+
   )
 }
 
